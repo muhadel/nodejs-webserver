b306a95879695f3d960798b853719b053cad4dff
Mohamed Adel <mohamed.adel@barqsystems.com>
2021-06-19 15:19:42 +0300

    add healthcheck route

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 // Constants
 const PORT = process.env.PORT || 8080;
 const HOST = process.env.HOST || '127.0.0.1';
+const ENV = process.env.NODE_ENV || 'development';
 
 // App
 const app = express();
@@ -14,8 +15,16 @@ const app = express();
 // Middleware
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Hello World');
+});
+app.get('/health', (_, res) => {
+  res.status(200).json({
+    uptime: process.uptime(),
+    message: 'OK',
+    timestamp: new Date().toISOString(),
+    config: { env: ENV, host: HOST, port: PORT },
+  });
 });
 
 app.listen(PORT, () => {
