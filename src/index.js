1461d93eb4628ef5216ff6d12c4a0c9dabf2ef94
Mohamed Adel <mohamed.adel@barqsystems.com>
2021-06-19 15:25:13 +0300

    move index.js to src directory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
 require('dotenv').config();
 
 // Constants
